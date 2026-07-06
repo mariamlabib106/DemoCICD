687f26b6dd401b6b0e2056c9163497b99bcd39e4
mariamlabib106 <mariammohamed106.com@gmail.com>
2026-07-07 01:58:44 +0300

    Initial Project

---
+++ DemoCICD/Controllers/WeatherController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoCICD.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WeatherController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                Message = "Hello from CI/CD Demo Project!",
+                Date = DateTime.Now
+            });
+        }
+    }
+}
