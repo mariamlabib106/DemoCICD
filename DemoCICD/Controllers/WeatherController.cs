19b59c7f0e182a849f70f7d37c4b26839bda55b2
mariamlabib106 <mariammohamed106.com@gmail.com>
2026-07-07 02:24:18 +0300

    Ignore Visual Studio files

--- DemoCICD/Controllers/WeatherController.cs
+++ DemoCICD/Controllers/WeatherController.cs
@@ -11,7 +11,7 @@ namespace DemoCICD.Controllers
         {
             return Ok(new
             {
-                Message = "Hello from CI/CD Demo Project!",
+                Message = "Pipeline Trigger Test",
                 Date = DateTime.Now
             });
         }
